3c369d82def1c692e4e763230fb850c7050ce4a7
ivan <ivanmoreno405@gmail.com>
2021-12-15 10:09:41 -0600

    microservice respuestas builded

---
+++ microservice-respuestas/src/test/java/com/ivanmoreno/respuestas/MicroserviceRespuestasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivanmoreno.respuestas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceRespuestasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
