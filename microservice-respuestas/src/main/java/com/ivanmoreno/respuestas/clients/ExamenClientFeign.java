46240ae17eb7001dac6e50fdd0500092eca10d65
ivan <ivanmoreno405@gmail.com>
2021-12-28 11:06:04 -0600

    get Examenes Id by Preguntas Id --> Feign Client Examenes

--- microservice-respuestas/src/main/java/com/ivanmoreno/respuestas/clients/ExamenClientFeign.java
+++ microservice-respuestas/src/main/java/com/ivanmoreno/respuestas/clients/ExamenClientFeign.java
@@ -1,8 +1,11 @@
 package com.ivanmoreno.respuestas.clients;
 
+import java.util.List;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ivanmoreno.commons.models.entity.Examen;
 
@@ -12,4 +15,6 @@ public interface ExamenClientFeign {
 	@GetMapping("/{id}")
 	public Examen getExamenById(@PathVariable Long id);
 	
+	@GetMapping("/respondidos-por-preguntas")
+	public List<Long> getExamenesIdsByPreguntasId(@RequestParam List<Long> preguntasId);
 }
